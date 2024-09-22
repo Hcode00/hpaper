1879255fbeab09f7b2304e6f8ffc458bc17b0088
Hcode00 <hcode1937@gmail.com>
2024-09-22 20:57:22 +0300

    refactored main function

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -6,11 +6,13 @@ import (
 	"path/filepath"
 	"strconv"
 	"syscall"
+	"time"
 
 	"github.com/sevlyar/go-daemon"
 
 	s "github.com/Hcode00/hpaper/service"
 	u "github.com/Hcode00/hpaper/utils"
+	w "github.com/Hcode00/hpaper/wallpapers"
 )
 
 var (
@@ -59,6 +61,96 @@ func StartDaemon(cntxt *daemon.Context, service *s.Hpaper) (*daemon.Context, err
 	}
 
 	return cntxt, nil
+}
+
+func Download() {
+	if len(os.Args) < 6 {
+		s.Help()
+		return
+	}
+	dir := os.Args[2]
+	numStr := os.Args[3]
+	width := os.Args[4]
+	height := os.Args[5]
+
+	isDir, err := u.IsDir(u.AbsPath(dir))
+	if err != nil {
+		u.LOG.Panic(err.Error())
+	}
+	if !isDir {
+		u.LOG.Panic("Please Specify a directory to save wallpapers in")
+	}
+	max, err := strconv.Atoi(numStr)
+	if err != nil || max < 0 {
+		u.LOG.Error(numStr + "not a valid or usable number")
+		return
+	}
+	if max < 1 || max > 20 {
+		u.LOG.Panic("Downloaded Range from 1 to 20")
+	}
+	isWebp := false
+	if len(os.Args) > 6 {
+		if os.Args[6] == "-w" {
+			isWebp = true
+		}
+	}
+	err = w.DownloadFile(u.AbsPath(dir), width, height, uint(max), isWebp)
+	if err != nil {
+		u.LOG.Panic(err.Error())
+	}
+}
+
+func StartApp(command string, service *s.Hpaper) {
+	arg2 := os.Args[2]
+	if len(os.Args) < 3 {
+		s.Help()
+		return
+	}
+	isDir, _ := u.IsDir(u.AbsPath(arg2))
+	if isDir {
+		seconds := os.Args[3]
+		maxLoaded := os.Args[4]
+
+		max, err := strconv.Atoi(maxLoaded)
+		if err != nil || max < 0 {
+			u.LOG.Error(maxLoaded + "not a valid or usable number")
+			return
+		}
+		sec, err := strconv.Atoi(seconds)
+		if err != nil || sec < 0 {
+			u.LOG.Error(maxLoaded + "not a valid or usable number of seconds")
+			return
+		}
+		isRandom := false
+		if len(os.Args) > 5 {
+			if os.Args[5] == "-r" {
+				isRandom = true
+			}
+		}
+		s.HandleSignals(command)
+
+		service = &s.Hpaper{
+			MaxToLoad:  uint(max),
+			CurrentIdx: 0,
+			Interval:   time.Duration(sec) * time.Second,
+			Path:       arg2,
+			Randomize:  isRandom,
+		}
+		ctx, err := StartDaemon(Cntxt, service)
+		if err != nil {
+			u.LOG.Panic("Unable to run ->" + err.Error())
+		}
+		defer u.LOG.Debug("Service Ended.")
+		defer ctx.Release()
+		defer RemovePidFile()
+	} else if u.IsValidPicture(arg2) {
+		u.StartHyprpaper()
+		// give hyprpaper time to launch
+		time.Sleep(1000 * time.Millisecond)
+		u.SetWallpaper(arg2)
+	} else {
+		u.LOG.Panic("Invalid Command")
+	}
 }
 
 func HandleExternalCommand(cntxt *daemon.Context, command string, service *s.Hpaper) {
--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"os"
-	"strconv"
-	"time"
 
 	d "github.com/Hcode00/hpaper/daemon"
 	s "github.com/Hcode00/hpaper/service"
-	u "github.com/Hcode00/hpaper/utils"
-	w "github.com/Hcode00/hpaper/wallpapers"
 )
 
 var service *s.Hpaper
@@ -24,94 +20,12 @@ func main() {
 	case "--help", "help":
 		s.Help()
 	case "start":
-		arg2 := os.Args[2]
-		if len(os.Args) < 3 {
-			s.Help()
-			return
-		}
-		isDir, _ := u.IsDir(u.AbsPath(arg2))
-		if isDir {
-			seconds := os.Args[3]
-			maxLoaded := os.Args[4]
-
-			max, err := strconv.Atoi(maxLoaded)
-			if err != nil || max < 0 {
-				u.LOG.Error(maxLoaded + "not a valid or usable number")
-				return
-			}
-			sec, err := strconv.Atoi(seconds)
-			if err != nil || sec < 0 {
-				u.LOG.Error(maxLoaded + "not a valid or usable number of seconds")
-				return
-			}
-			isRandom := false
-			if len(os.Args) > 5 {
-				if os.Args[5] == "-r" {
-					isRandom = true
-				}
-			}
-			s.HandleSignals(command)
-
-			service = &s.Hpaper{
-				MaxToLoad:  uint(max),
-				CurrentIdx: 0,
-				Interval:   time.Duration(sec) * time.Second,
-				Path:       arg2,
-				Randomize:  isRandom,
-			}
-			ctx, err := d.StartDaemon(d.Cntxt, service)
-			if err != nil {
-				u.LOG.Panic("Unable to run ->" + err.Error())
-			}
-			defer u.LOG.Debug("Service Ended.")
-			defer ctx.Release()
-			defer d.RemovePidFile()
-		} else if u.IsValidPicture(arg2) {
-			u.StartHyprpaper()
-			// give hyprpaper time to launch
-			time.Sleep(1000 * time.Millisecond)
-			u.SetWallpaper(arg2)
-		} else {
-			u.LOG.Panic("Invalid Command")
-		}
+		d.StartApp(command, service)
 
 	case "next", "prev", "status":
 		d.HandleExternalCommand(d.Cntxt, command, service)
 	case "download":
-		if len(os.Args) < 6 {
-			s.Help()
-			return
-		}
-		dir := os.Args[2]
-		numStr := os.Args[3]
-		width := os.Args[4]
-		height := os.Args[5]
-
-		isDir, err := u.IsDir(u.AbsPath(dir))
-		if err != nil {
-			u.LOG.Panic(err.Error())
-		}
-		if !isDir {
-			u.LOG.Panic("Please Specify a directory to save wallpapers in")
-		}
-		max, err := strconv.Atoi(numStr)
-		if err != nil || max < 0 {
-			u.LOG.Error(numStr + "not a valid or usable number")
-			return
-		}
-		if max < 1 || max > 20 {
-			u.LOG.Panic("Downloaded Range from 1 to 20")
-		}
-		isWebp := false
-		if len(os.Args) > 6 {
-			if os.Args[6] == "-w" {
-				isWebp = true
-			}
-		}
-		err = w.DownloadFile(u.AbsPath(dir), width, height, uint(max), isWebp)
-		if err != nil {
-			u.LOG.Panic(err.Error())
-		}
+		d.Download()
 	case "quit":
 		d.SendQuit()
 	default:
